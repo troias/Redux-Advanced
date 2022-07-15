e27943aaef448f6a17ed44ec0aa864af8a190c7e
troy <hunter007nz@gmail.com>
2022-07-15 23:42:51 +1200

    added env vars

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import Cart from './components/Cart/Cart';
-import { useEffect } from 'react';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import Notification from './components/UI/Notification'
-import { useSelector, useDispatch } from 'react-redux'
-import { sendCartData, fetchCartData } from './store/cart-actions'
+import Cart from "./components/Cart/Cart"
+import { useEffect } from "react"
+import Layout from "./components/Layout/Layout"
+import Products from "./components/Shop/Products"
+import Notification from "./components/UI/Notification"
+import { useSelector, useDispatch } from "react-redux"
+import { sendCartData, fetchCartData } from "./store/cart-actions"
 let isInitial = true
 
 function App() {
-  const cart = useSelector(state => state.cart)
-  const showCart = useSelector(state => state.uiSlice.showCart)
-  const notificationStatus = useSelector(state => state.uiSlice.notification)
- 
+  const cart = useSelector((state) => state.cart)
+  const showCart = useSelector((state) => state.uiSlice.showCart)
+  const notificationStatus = useSelector((state) => state.uiSlice.notification)
+
   const dispatch = useDispatch()
   console.log("cartState", cart)
+
   useEffect(() => {
     dispatch(fetchCartData())
-   }, [dispatch])
+  }, [dispatch])
 
   useEffect(() => {
-
     if (isInitial) {
       isInitial = false
       return
@@ -27,25 +27,23 @@ function App() {
     if (cart.changed) {
       dispatch(sendCartData(cart))
     }
-   
-
-  }, [cart, dispatch]);
+  }, [cart, dispatch])
 
   return (
     <>
-      {notificationStatus && <Notification
-        status={notificationStatus.status}
-        title={notificationStatus.title}
-        message={notificationStatus.message}
-      />}
+      {notificationStatus && (
+        <Notification
+          status={notificationStatus.status}
+          title={notificationStatus.title}
+          message={notificationStatus.message}
+        />
+      )}
       <Layout>
-
         {showCart && <Cart />}
         <Products />
-
       </Layout>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,74 +1,81 @@
-import { uiActions } from './ui'
-import {cartActions} from './cart'
+import { uiActions } from "./ui"
+import { cartActions } from "./cart"
 
 export const sendCartData = (cart) => {
-    return async (dispatch) => {
+  return async (dispatch) => {
+    //side effect
+    dispatch(
+      uiActions.showNotification({
+        status: "Pending-request",
+        title: "Sending",
+        message: "Sending cart data!",
+      })
+    )
 
-        //side effect
-        dispatch(uiActions.showNotification({
-            status: "Pending-request",
-            title: "Sending",
-            message: "Sending cart data!"
-        }))
-
-        const sendRequest = async () => {
-            const req = await fetch("https://react-adv-38f1a-default-rtdb.firebaseio.com/cart.json", {
-                method: "PUT",
-                body: JSON.stringify({
-                    items: cart.cartItems,
-                    totalCartItemQuantity: cart.totalCartItemQuantity,
-                }),
-
-            })
-            if (!req.ok) {
-                throw new Error('sending cart data failed')
-            }
-            dispatch(uiActions.showNotification({
-                status: "success",
-                title: "Success",
-                message: "Sent cart data successfully "
-            }))
-        }
-        try {
-             await sendRequest()
-        } catch (error) {
-            dispatch(uiActions.showNotification({
-                status: "error",
-                title: error.title,
-                message: error.message
-            }))
-        }
+    const sendRequest = async () => {
+      const req = await fetch(process.env.NEXT_PUBLIC_FIREBASE_API, {
+        method: "PUT",
+        body: JSON.stringify({
+          items: cart.cartItems,
+          totalCartItemQuantity: cart.totalCartItemQuantity,
+        }),
+      })
+      if (!req.ok) {
+        throw new Error("sending cart data failed")
+      }
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent cart data successfully ",
+        })
+      )
     }
+    try {
+      await sendRequest()
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: error.title,
+          message: error.message,
+        })
+      )
+    }
+  }
 }
 
 export const fetchCartData = () => {
-    return async (dispatch) => {
-    
-        const reqData = async () => {
-            const req = await fetch("https://react-adv-38f1a-default-rtdb.firebaseio.com/cart.json")
-            if (!req.ok) {
-                throw new Error('sending cart data failed')
-            }
-            const res = await req.json()
-          
-            return res
-        }
-        try {
-            const cartData = await reqData()
-            console.log("cartData", cartData)
-            if (!cartData) {
-                throw new Error('Add Item to cart')
-            }
-            dispatch(cartActions.replaceCart({
-                cartItems:  cartData.cartItems || [],
-                totalCartItemQuantity: cartData.totalCartItemQuantity || 0
-            }))
-        } catch (error) {
-            dispatch(uiActions.showNotification({
-                status: "error",
-                title: error.title,
-                message: error.message
-            }))
-        }
+  return async (dispatch) => {
+    const reqData = async () => {
+      const req = await fetch(process.env.NEXT_PUBLIC_FIREBASE_API)
+      if (!req.ok) {
+        throw new Error("sending cart data failed")
+      }
+      const res = await req.json()
+
+      return res
     }
+    try {
+      const cartData = await reqData()
+      console.log("cartData", cartData)
+      if (!cartData) {
+        throw new Error("Add Item to cart")
+      }
+      dispatch(
+        cartActions.replaceCart({
+          cartItems: cartData.cartItems || [],
+          totalCartItemQuantity: cartData.totalCartItemQuantity || 0,
+        })
+      )
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: error.title,
+          message: error.message,
+        })
+      )
+    }
+  }
 }
